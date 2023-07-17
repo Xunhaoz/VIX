19864109826c2bcc7aba3fd9e9c787d6c98cf54b
Xunhaoz <leo20020529@gmail.com>
2023-07-17 22:43:15 +0800

    update requirement

--- predict_list.py
+++ predict_list.py
@@ -1,11 +1,11 @@
+import tensorflow as tf
 import train_lstm as tl
-
 from tensorflow import keras
 
 adj_close_df = tl.get_stock(['TSM', "^TWII"])
 all_data_x, all_data_y, scalers = tl.preprocess(adj_close_df, rolling=60)
 
-model = keras.models.load_model('500epochs.h5')
+model = keras.models.load_model('500epochs.h5', compile=False)
 predictions = model.predict(all_data_x)
 
-tl.result(predictions, all_data_y, scalers)
+tl.result(predictions[:1000], all_data_y[:1000], scalers)
